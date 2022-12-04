10db4bdf6b3bdbcb6a649fbbaddee3b2551230cc
minu1117 <108320084+minu1117@users.noreply.github.com>
2022-12-04 23:53:40 +0900

    보스 Hp Bar 버그 수정

--- WizardOfLegend/WizardOfLegend/Ui/PlayUiMgr.cpp
+++ WizardOfLegend/WizardOfLegend/Ui/PlayUiMgr.cpp
@@ -526,7 +526,10 @@ void PlayUiMgr::BossHpBraSizeControl(float dt)
 		// HP Yellow Bar Control
 		if (bossCurHpBarSet < bossHpBarHurtSize)
 		{
-			bossHpBarHurt->SetSize({ bossHpBarHurtSize -= (dt * 50), bossHpBarHurt->GetSize().y * 4.f });
+			if (bossHpBarHurt->GetSize().x >= 0.f)
+				bossHpBarHurt->SetSize({ bossHpBarHurtSize -= (dt * 50), bossHpBarHurt->GetSize().y * 4.f });
+			else
+				bossHpBarHurt->SetSize({ 0.f, bossHpBarHurt->GetSize().y * 4.f });
 		}
 
 		if (bossCurHp <= 0 && isAlive)
@@ -535,6 +538,7 @@ void PlayUiMgr::BossHpBraSizeControl(float dt)
 			if (dieTimer <= 0.f)
 			{
 				uiObjList[2].clear();
+				bossHpBarHurtSize = 89.f * 4.f;
 				isAlive = false;
 				isStart = false;
 			}
